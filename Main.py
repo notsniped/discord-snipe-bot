a38f5d01d245d211dd8a4ab74c23340f86c0a4b4
snipe_blaze <72265661+notsniped@users.noreply.github.com>
2022-10-03 15:37:47 +0530

    Implement logging for all message delete and edit events

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
-import os, time, os.path, discord, datetime, json
+import os, time, os.path, discord, json
+from datetime import datetime
 from discord.ext import commands
 from discord_slash import SlashCommand, SlashContext
 from discord_slash.utils.manage_commands import create_choice, create_option
@@ -45,6 +46,47 @@ if os.name == "posix":
     open(f"{homedir}/snipe-bot-data/editsnipe.log", 'x')
     open(f"{homedir}/snipe-bot-data/errors.log", 'x')
 
+
+class Log:
+    def __init__(self, os_name: str, directory: str):
+        self.os_name = os_name
+        self.directory = directory
+        start_timestamp = datetime.now().strftime('%H:%M:%S')
+        print(f"[{start_timestamp}] Logging initialized.")
+
+    def snipe(self, text: str):
+        if os.name == "nt":
+            with open(f"{homedir}\\snipe-bot-data\\snipe.log", 'w') as file:
+                # timestamp = datetime.now().strftime("%H:%M:%S")  Disable internal timestamp logging
+                file.write(text)
+        elif os.name == "posix":
+            with open(f"{homedir}/snipe-bot-data/snipe.log", 'w') as file:
+                # timestamp = datetime.now().strftime("%H:%M:%S")  Disable internal timestamp logging
+                file.write(text)
+
+    def editsnipe(self, text: str):
+        if os.name == "nt":
+            with open(f"{homedir}\\snipe-bot-data\\editsnipe.log", 'w') as file:
+                # timestamp = datetime.now().strftime("%H:%M:%S")  Disable internal timestamp logging
+                file.write(text)
+        elif os.name == "posix":
+            with open(f"{homedir}/snipe-bot-data/editsnipe.log", 'w') as file:
+                # timestamp = datetime.now().strftime("%H:%M:%S")  Disable internal timestamp logging
+                file.write(text)
+
+    def error(self, text: str):
+        if os.name == "nt":
+            with open(f"{homedir}\\snipe-bot-data\\errors.log", 'w') as file:
+                # timestamp = datetime.now().strftime("%H:%M:%S")  Disable internal timestamp logging
+                file.write(text)
+        elif os.name == "posix":
+            with open(f"{homedir}/snipe-bot-data/errors.log", 'w') as file:
+                # timestamp = datetime.now().strftime("%H:%M:%S")  Disable internal timestamp logging
+                file.write(text)
+
+
+logger = Log(os.name, homedir)
+
 @client.command()
 async def help(ctx):
     e = discord.Embed(title='Command Help', description=f'Prefix: `{prefix}`\n\n`{str(prefix)}snipe`: See the most recently deleted message in this channel.\n`{str(prefix)}editsnipe`: See the most recently edited message in this channel.', color=discord.Color.random())
@@ -74,7 +116,10 @@ async def on_message_delete(message):
         channel = message.channel
         snipe_message_author[message.channel.id] = message.author
         snipe_message_content[message.channel.id] = message.content
-        if bool(snipe_log): print(f"Message deleted in #{channel} ({message.guild}):\n   Message content: {message.content}")
+        if bool(snipe_log):
+            timestamp = datetime.now().strftime('%H:%M:%S')
+            print(f"[{timestamp}] Message deleted in #{channel} ({message.guild}):\n   Message content: {message.content}")
+            logger.snipe(f"[{timestamp}] Message deleted in #{channel} ({message.guild}): {message.content}")
         else: pass
 
 @client.event
@@ -85,7 +130,10 @@ async def on_message_edit(message_before, message_after):
         channel = message_before.channel
         editsnipe_message_before_content[channel.id] = message_before.content
         editsnipe_message_after_content[channel.id] = message_after.content
-        if bool(editsnipe_log): print(f"Message edited in #{channel} ({message_before.guild}):\n   Old message: {message_before.content}\n   New message: {message_after.content}")
+        if bool(editsnipe_log):
+            timestamp = datetime.now().strftime('%H:%M:%S')
+            print(f"[{timestamp}] Message edited in #{channel} ({message_before.guild}):\n   Old message: {message_before.content}\n   New message: {message_after.content}")
+            logger.editsnipe(f"[{timestamp}] Message edited in #{channel} ({message_before.guild}):\n   Old message: {message_before.content}\n   New message: {message_after.content}")
         else: pass
 
 @client.command()
