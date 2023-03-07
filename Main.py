48548bbdefc0a4732bc294adf24f759d31ff5a6c
snipe <72265661+notsniped@users.noreply.github.com>
2023-03-07 13:24:02 +0000

    Clean up imports

--- Main.py
+++ Main.py
@@ -7,10 +7,7 @@ import json
 import framework.auth
 from datetime import datetime
 from discord.ext import commands
-from discord_slash import SlashCommand, SlashContext
-from discord_slash.utils.manage_commands import create_choice, create_option
 from discord.ext.commands import *
-from discord.ext import tasks
 from framework.logger import Logger
 
 # User Configuration (edit stuff here)
