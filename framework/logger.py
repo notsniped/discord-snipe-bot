bcd2fd17905a20c577026ea95b998cac420ff5c9
snipe <72265661+notsniped@users.noreply.github.com>
2024-04-05 09:36:31 +0530

    Improve docstring for `framework.Logger` class

--- framework/logger.py
+++ framework/logger.py
@@ -5,10 +5,10 @@ import datetime
 
 # Classes and Functions
 class Logger:
-    """A class used for logging important information and alerts."""
     def __init__(self, os_name: str, directory: str):
         self.os_name = os_name
         self.directory = directory
+    """A class used for logging important new snipe and editsnipe entries, as well as errors."""
         start_timestamp = datetime.time().strftime('%H:%M:%S')
         print(f"[{start_timestamp}] Logging initialized.")
 
