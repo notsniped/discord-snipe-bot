fa7705b7d98e23c8a5c3abbc291a47ed976fcc9d
snipe <72265661+notsniped@users.noreply.github.com>
2024-04-30 23:54:22 +0530

    Add a framework module to fetch terminal colors

---
+++ framework/colors.py
@@ -0,0 +1,10 @@
+"""Library used for fetching stdout colors. (color codes)"""
+
+class Colors:
+    """Contains general stdout colors."""
+    cyan = '\033[96m'
+    red = '\033[91m'
+    green = '\033[92m'
+    orange = '\033[33m'
+    yellow = '\033[93m'
+    end = '\033[0m'
